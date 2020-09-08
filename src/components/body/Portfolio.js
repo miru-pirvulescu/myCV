9a72ea4b76881c337157e3b8f7891afb50bf308c
Miruna Pirvulescu <32644201+miru-pirvulescu@users.noreply.github.com>
2020-09-08 17:50:16 +0300

    refactor portfolio

--- src/components/body/Portfolio.js
+++ src/components/body/Portfolio.js
@@ -1,51 +1,42 @@
-import React from 'react';
+import React from "react";
 import Project from "./Project";
-import cat1 from "../../assets/img/cat1.jpg"
-import List from '@material-ui/core/List';
+import {CardDeck} from 'react-bootstrap';
+
+const projects = [
+  { id: 1, name: "Escape", description: "How easily will you escape this perfect summer retreat?", thumbnail: "", tags: ["UE4", "C++"]},
+  { id: 2, name: "Bulls & Cows", description: "Guess the isogram, based on the given bulls and cows.", thumbnail: "", tags: ["UE4", "C++"]},
+  { id: 3, name: "GoGrocer", description: "Automating your path through a grocery store; now you can find it all!", thumbnail: "", tags: ["React Native"]},
+  { id: 4, name: "The Enchanted Chest", description: "Are you bored enough to want to defeat the chest and get your promised prize?", thumbnail: "", tags: ["C++"]},
+  { id: 5, name: "A-Maze", description: "A Java-based game developed during Code for Good by JP Morgan.", thumbnail: "", tags: ["Java", "Java Swing"]},
+  { id: 6, name: "This website!", description: "Made from scratch with some basics and some online documentation.", thumbnail: "", tags: ["React.js"]}
+];
+
+const portfoliostyle = {
+    background: "rgba(0,0,0,0.9)",
+    paddingLeft: "40px",
+    paddingRight: "40px",
+    paddingBottom: "40px",
+    paddingTop: "1px",
+    fontFamily: "calibri",
+}
 
 const Portfolio = () => {
-    return(
+    return (
         <>
-            <h3>Portfolio</h3>
-
-            <List style={{
-                justifyContent: "space-between",
-                display: 'flex',
-                flexDirection: 'row',
-                padding: '20px',
-                background:'black'
-            }} dense>
-                <Project 
-                    title='A-Maze!'
-                    description="this is a cat"
-                    thumbnail={cat1}
-                />
-                <Project 
-                    title='GoGrocer'
-                    description="this is a cat"
-                    thumbnail={cat1}
-                />
-                <Project 
-                    title='Enchanted Chest'
-                    description="this is a cat"
-                    thumbnail={cat1}
-                />
-                <Project 
-                    title='Bulls and Cows'
-                    description="this is a cat"
-                    thumbnail={cat1}
-                />
-                <Project 
-                    title='Escape'
-                    description="this is a cat"
-                    thumbnail={cat1}
-                />
-                <Project 
-                    title='This website!'
-                    description="this is a cat"
-                    thumbnail={cat1}
-                />
-            </List>
+            <div style={portfoliostyle}>
+                <h3 style={{color: "white"}}>Portfolio</h3>
+                
+                <CardDeck>
+                    {projects.map(project => (
+                    <Project 
+                        title={project.name}
+                        description={project.description}
+                        thumbnail={process.env.PUBLIC_URL + '/cat2.jpg'}
+                        tags={project.tags}
+                    />
+                    ))}
+                </CardDeck>
+            </div>
         </>
     );
 }
